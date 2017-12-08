b4cd9da95794b9591dd12475d6bdc57cf8d98e82
Anastasiia_Rybakova <anastasiia_rybakova@epam.com>
2017-12-08 13:02:08 +0300

    Task 2

--- src/main/test/Fibonacci.java
+++ src/main/test/Fibonacci.java
@@ -1,11 +1,44 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //Task 2. O(n)
 public class Fibonacci {
 
+    public static int max(int[] array) {
+        int maximum = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (maximum < array[i]) maximum = array[i];
+        }
+        return maximum;
+    }
+
+    public static List<Integer> fib(int max, List<Integer> fib) {
+        if (max == 0) {
+            fib.add(0);
+            return fib;
+        }
+        int a = 0;
+        int b = 1;
+        int c = a + b;
+        fib.add(a);
+        fib.add(b);
+        while (c < max) {
+            c = a + b;
+            a = b;
+            b = c;
+            fib.add(c);
+        }
+        return fib;
+    }
+
     public static boolean isFib(int[] array) {
-        for (int i = 2; i < array.length; i++) {
-            if (array[i] != array[i - 1] + array[i - 2]) {
+        int max = max(array);
+        List<Integer> list = new ArrayList<>();
+        List<Integer> fib = fib(max, list);
+        for (int i = 0; i < array.length; i++) {
+            if (!fib.contains(array[i])) {
                 return false;
             }
         }
@@ -14,10 +47,14 @@ public class Fibonacci {
 
     public static void main(String[] args) {
         int[] array = {0, 0, 1, 2, 3, 5};
-        System.out.println(isFib(array));
         int[] array2 = {1, 2, 3};
-        System.out.println(isFib(array2));
         int[] array3 = {2, 2, 3, 5};
+        int[] array4 = {2, 2, 3, 5, 7};
+
+        System.out.println(isFib(array));
+        System.out.println(isFib(array2));
         System.out.println(isFib(array3));
+        System.out.println(isFib(array4));
+
     }
 }
