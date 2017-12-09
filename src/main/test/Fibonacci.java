8ef8ad03cbb1b99e3616c74f901c0e4324172bb3
Anastasiia_Rybakova <anastasiia_rybakova@epam.com>
2017-12-09 23:41:16 +0300

    Task 1. Fibonacci

--- src/main/test/Fibonacci.java
+++ src/main/test/Fibonacci.java
@@ -1,61 +1,71 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 //Task 2. O(n)
 public class Fibonacci {
 
-    public static int max(int[] array) {
-        int maximum = array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (maximum < array[i]) {
-                maximum = array[i];
+    public static boolean isFib(String seq) {
+        int i = 0;
+        int step = 1;
+
+        while (i <= seq.length() - 3 * step) {
+            int cur = subint(seq, i, i + step);
+            int next = subint(seq, i + step, i + 2 * step);
+            int sum = subint(seq, i + 2 * step, i + 3 * step);
+            int next2 = subint(seq, i + step, i + 2 * step + 1);
+
+            if (cur + next == sum) {
+                if (i + 3 * step == seq.length()) {
+                    return true;
+                }
+                i += step;
+            } else if (i <= seq.length() - 3 * step - 2 &&
+                    cur + next2 == subint(seq, i + 2 * step + 1, i + 3 * step + 2)) {
+                if (i + 3 * step + 2 == seq.length()) {
+                    return true;
+                }
+                step++;
+                i += step;
+            } else if (i <= seq.length() -  3 * step - 1 &&
+                    cur + next == subint(seq, i + 2 * step, i + 3 * step + 1)) {
+                if (i + 3 * step + 1 == seq.length()) {
+                    return true;
+                }
+                i += step;
+                step++;
+            } else {
+                i = 0;
+                step++;
             }
         }
-        return maximum;
+        return false;
     }
 
-    public static List<Integer> fib(int max) {
-        List<Integer> fib = new ArrayList<>();
-        if (max == 0) {
-            fib.add(0);
-            return fib;
-        }
-        int a = 0;
-        int b = 1;
-        int c = a + b;
-        fib.add(a);
-        fib.add(b);
-        while (c < max) {
-            c = a + b;
-            a = b;
-            b = c;
-            fib.add(c);
-        }
-        return fib;
-    }
-
-    public static boolean isFib(int[] array) {
-        List<Integer> fib = fib(max(array));
-        for (int i = 0; i < array.length; i++) {
-            if (!fib.contains(array[i])) {
-                return false;
-            }
-        }
-        return true;
+    public static int subint(String str, int start, int end) {
+        return Integer.parseInt(str.substring(start, end));
     }
 
     public static void main(String[] args) {
-        int[] array = {0, 0, 1, 2, 3, 5};
-        int[] array2 = {1, 2, 3};
-        int[] array3 = {2, 2, 3, 5};
-        int[] array4 = {2, 2, 3, 5, 7};
-
-        System.out.println(isFib(array));
-        System.out.println(isFib(array2));
-        System.out.println(isFib(array3));
-        System.out.println(isFib(array4));
-
+        String example = "1234";
+        String example2 = "1235";
+        String example3 = "13213455";
+        String example4 = "13213457";
+        String example5 = "377610987";
+        String example6 = "377610988";
+        String example7 = "5813";
+        String example8 = "81321";
+        String example9 = "81322";
+        String example10 = "67651094617711";
+        String example11 = "676510946177110";
+        System.out.println(isFib(example)); //false
+        System.out.println(isFib(example2)); //true
+        System.out.println(isFib(example3)); //true
+        System.out.println(isFib(example4)); //false
+        System.out.println(isFib(example5)); //true
+        System.out.println(isFib(example6)); //false
+        System.out.println(isFib(example7)); //true
+        System.out.println(isFib(example8)); //true
+        System.out.println(isFib(example9)); //false
+        System.out.println(isFib(example10)); //true
+        System.out.println(isFib(example11)); //false
     }
 }
